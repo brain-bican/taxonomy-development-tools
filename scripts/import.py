518699dc1e7cebc3ef79d991d926ba18ad4472b1
hkir-dev <huseyin_kir@yahoo.com>
2023-07-17 09:48:00 +0100

    Loaded std data

--- scripts/import.py
+++ scripts/import.py
@@ -2,6 +2,7 @@ import os
 import csv
 import click
 import shutil
+import pandas as pd
 from ctat.cell_type_annotation import format_data
 
 
@@ -25,15 +26,54 @@ def import_data(input, schema, curation_tables):
     for filename in os.listdir(input):
         f = os.path.join(input, filename)
         if os.path.isfile(f):
-            if filename.endswith(".tsv") or filename.endswith(".csv"):
+            if (filename.endswith(".tsv") or filename.endswith(".csv")) and "_std." not in filename:
                 user_data_path = f
             elif filename.endswith(".yaml") or filename.endswith(".yml"):
                 user_config_path = f
 
-    user_data_ct_path = copy_file(user_data_path, curation_tables)
-    user_table_name = os.path.splitext(os.path.basename(user_data_path))[0]
+    add_user_table_to_nanobot(user_data_path, schema, curation_tables)
 
-    table_tsv_path = os.path.join(schema, "table.tsv")
+    user_file_name = os.path.splitext(os.path.basename(user_data_path))[0]
+    std_data = format_data(user_data_path, user_config_path, os.path.join(input, user_file_name + ".json"))
+
+    std_data_path = convert_standard_json_to_table(std_data, user_file_name, input)
+    add_user_table_to_nanobot(std_data_path, schema, curation_tables)
+
+
+def convert_standard_json_to_table(std_data: dict, source_file_name: str, input_folder: str):
+    """
+    Converts the standardized data json to tsv to be represented by nanobot.
+    Parameters:
+        std_data: standard json data content as dictionary
+        source_file_name: name of the user file
+        input_folder: tdt input data folder path
+    """
+    std_data_path = os.path.join(input_folder, source_file_name + "_std.tsv")
+    std_records = list()
+    for annotation_object in std_data["annotationObjects"]:
+        if "accessionId" in annotation_object:
+            record = dict()
+            record["accessionId"] = annotation_object["accessionId"]
+            record["cellLabel"] = annotation_object["cellLabel"]
+            record["parentNodeName"] = annotation_object["parentNodeName"]
+            record["markerGenes"] = annotation_object["markerGenes"]
+            if "userAnnotations" in annotation_object:
+                for user_annot in annotation_object["userAnnotations"]:
+                    record[normalize_column_name(user_annot["annotationSet"])] = user_annot["cellLabel"]
+            std_records.append(record)
+    std_records_df = pd.DataFrame.from_records(std_records)
+    std_records_df.to_csv(std_data_path, sep="\t", index=False)
+    return std_data_path
+
+
+def add_user_table_to_nanobot(user_data_path, schema_folder, curation_tables_folder):
+    """
+    Adds user data to the nanobot. Adds user table to the curation tables folder and updates the nanobot table schema.
+    """
+    # update nanobot table.tsv
+    user_data_ct_path = copy_file(user_data_path, curation_tables_folder)
+    user_table_name = os.path.splitext(os.path.basename(user_data_ct_path))[0]
+    table_tsv_path = os.path.join(schema_folder, "table.tsv")
     with open(table_tsv_path, 'a') as fd:
         fd.write(('\n{table_name}\t{path}\t\t\t').format(table_name=user_table_name, path=user_data_ct_path))
 
@@ -45,7 +85,8 @@ def import_data(input, schema, curation_tables):
     elif user_data_extension == ".csv":
         user_headers, user_data = read_csv_to_dict(user_data_ct_path, generated_ids=True)
 
-    column_tsv_path = os.path.join(schema, "column.tsv")
+    # update nanobot column.tsv
+    column_tsv_path = os.path.join(schema_folder, "column.tsv")
     with open(column_tsv_path, 'a') as fd:
         for index, header in enumerate(user_headers):
             # TODO fix assumption, first column is the id column
