2785cb1f19b9c4969b5fe8a566ad7ebe9fa74d69
hkir-dev <huseyin_kir@yahoo.com>
2024-06-14 14:56:11 +0100

    Title bar show taxonomy name #105

--- tdt/api/tdt_info.py
+++ tdt/api/tdt_info.py
@@ -1,3 +1,4 @@
+import subprocess
 from ruamel.yaml import YAML
 
 INFO_FILE_PATH = "/tools/tdt_info.yaml"
@@ -8,8 +9,9 @@ def get_tdt_info():
     Get the tdt info from config file.
     :return: tdt_info
     """
-    # Read the tdt info from the config file
-    return read_yaml_config(INFO_FILE_PATH)
+    tdt_info = read_yaml_config(INFO_FILE_PATH)
+    tdt_info["repo_url"] = get_repo_info()
+    return tdt_info
 
 
 def read_yaml_config(file_path: str) -> dict:
@@ -24,3 +26,14 @@ def read_yaml_config(file_path: str) -> dict:
             return ryaml.load(fs)
         except Exception as e:
             raise Exception("Yaml read failed:" + file_path + " " + str(e))
+
+
+def get_repo_info():
+    """
+    Get the origin repo from git.
+    :return: configured repository url
+    """
+    origin_url = subprocess.check_output(["git", "config", "--get", "remote.origin.url"]).strip().decode()
+    # parse https://token@github.com/repo_path.git/
+    repo_url = "https://www." + origin_url.split("@")[-1].split(".git")[0]
+    return repo_url
